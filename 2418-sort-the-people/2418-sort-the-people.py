6bd796ff4408e5966dc26c19c2529ba4fc03010e
Ahmed Alhassan <39542343+Ahmed-5@users.noreply.github.com>
2024-02-27 21:43:20 +0200

    Time: 746 ms (5.36%), Space: 24.6 MB (10.54%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,15 +1,14 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
         n = len(names)
-        for i in range(1, n):
-            pivot = heights[i]
-            for j in range(i-1, -1, -1):
-                if heights[j]<pivot:
-                    heights[j], heights[j+1] = heights[j+1], heights[j]
-                    names[j], names[j+1] = names[j+1], names[j]
-                else:
-                    break
+        l = [[] for i in range(100001)]
+        for i in range(n):
+            l[heights[i]].append(names[i])
+            
+        ns = []
+        for i in l[::-1]:
+            ns.extend(i)
                     
-        return names
+        return ns
                     
         
