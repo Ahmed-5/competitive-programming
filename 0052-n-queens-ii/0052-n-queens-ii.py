7eaaac6ec347670ae760fd4bbb18e054a18b19d7
Ahmed Alhassan <39542343+Ahmed-5@users.noreply.github.com>
2024-06-07 22:49:28 +0300

    Time: 111 ms (8.63%), Space: 16.8 MB (5.51%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.py
@@ -0,0 +1,47 @@
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        ans = []
+        sol = []
+        for i in range(n):
+            sol.append([])
+            for j in range(n):
+                sol[-1].append(".")
+                
+        def placable(n, r, c):
+            for i in range(n):
+                if sol[i][c] == "Q" or sol[r][i]=="Q":
+                    return False
+                
+                if c-i>-1 and r-i>-1 and sol[r-i][c-i]=="Q":
+                    return False
+                
+                if c-i>-1 and r+i<n and sol[r+i][c-i]=="Q":
+                    return False
+                
+                if c+i<n and r-i>-1 and sol[r-i][c+i]=="Q":
+                    return False
+                
+                if c+i<n and r+i<n and sol[r+i][c+i]=="Q":
+                    return False
+                
+            return True
+        
+        def solve(n, r=0):
+            if r==n:
+                added = ["".join(i) for i in sol]
+                ans.append(added)
+                return
+            
+            placed = True
+            for c in range(n):
+                placed = placable(n, r, c)
+                if not placed:
+                    continue
+                    
+                sol[r][c] = "Q"
+                solve(n, r+1)
+                sol[r][c] = "."
+            
+        solve(n)
+        
+        return len(ans)
