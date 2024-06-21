40e6484b977e8878d02342be473fa8a5e93b9bbe
Ahmed Alhassan <39542343+Ahmed-5@users.noreply.github.com>
2024-06-21 21:46:33 +0300

    Time: 1824 ms (11.85%), Space: 16.7 MB (77.58%) - LeetHub

---
+++ 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.py
@@ -0,0 +1,44 @@
+from math import sqrt
+
+class Solution:
+    def maximumDetonation(self, bombs: List[List[int]]) -> int:
+        def isIntersected(circle1, circle2):
+            x1, y1, r1 = circle1
+            x2, y2, r2 = circle2
+            dist = sqrt((x1-x2)**2 + (y1-y2)**2)
+            return r1>=dist, r2>=dist
+        
+        n = len(bombs)
+        graph = [[0 for j in range(n)] for i in range(n)]
+            
+        for i in range(n):
+            for j in range(i, n):
+                circle1 = bombs[i]
+                circle2 = bombs[j]
+                outwards, inwards = isIntersected(circle1, circle2)
+                if outwards:
+                    graph[i][j] = 1
+                if inwards:
+                    graph[j][i] = 1
+                    
+        for i in range(n):
+            for j in range(n):
+                if j==i or graph[i][j]==0:
+                    continue
+
+                for k in range(n):
+                    if graph[j][k]==1:
+                        graph[i][k]=1
+                    
+        for i in range(n):
+            for j in range(n):
+                if j==i or graph[i][j]==0:
+                    continue
+
+                for k in range(n):
+                    if graph[j][k]==1:
+                        graph[i][k]=1
+                            
+        intersections = [sum(i) for i in graph]
+            
+        return max(intersections)
