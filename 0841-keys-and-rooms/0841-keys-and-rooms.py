71940337ca53efa3c999c4eb12d5e945f3c3c257
Ahmed Alhassan <39542343+Ahmed-5@users.noreply.github.com>
2024-06-26 23:57:38 +0300

    Time: 64 ms (63.67%), Space: 17 MB (79.44%) - LeetHub

---
+++ 0841-keys-and-rooms/0841-keys-and-rooms.py
@@ -0,0 +1,13 @@
+class Solution:
+    def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+        lookup = set([0])
+        stack = [0]
+        while stack:
+            node = stack.pop()
+            for nei in rooms[node]:
+                if nei not in lookup:
+                    lookup.add(nei)
+                    if len(lookup) == len(rooms):
+                        return True
+                    stack.append(nei)
+        return len(lookup) == len(rooms)
